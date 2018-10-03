3edef774c5989f46b7a9adf4440f984dba23dbb1
leoperz7 <jleoperz@gmail.com>
2018-10-03 00:13:27 -0300

    primer commir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/select/select.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';    
+
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.scss']
+})
+export class SelectComponent implements OnInit {
+
+dropdownList = [];
+selectedItems = [];
+dropdownSettings = {};
+ 
+
+
+  constructor() {
+    
+    
+      
+
+   }
+
+  ngOnInit() {
+
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      { item_id: 2, item_text: 'Bangaluru' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+      { item_id: 5, item_text: 'New Delhi' }
+    ];
+    this.selectedItems = [
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' }
+    ];
+
+    this.dropdownSettings ={
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+
+    
+    }
+
+
+    public onItemSelect (item:any) {
+      console.log(item);
+}
+
+    public onSelectAll (items: any) {
+            console.log(items);
+          }
+
+        }
